51d65259dbfd954da67ab1dbd8ada5c45a4193f5
emrekardaslar <emre.kardaslar@gmail.com>
2023-05-20 16:52:07 +0300

    add timeout for fitting markers

--- components/Map.js
+++ components/Map.js
@@ -15,9 +15,11 @@ const Map = () => {
   useEffect(() => {
     if (!origin || !destination) return;
     //zoom implement if there's an origin and destination
-    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
-      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
-    });
+    setTimeout(() => {
+      mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+        edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+      });
+    }, 100);
   }, [origin, destination]);
 
   return (

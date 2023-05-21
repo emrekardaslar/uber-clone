b4e7e35f00e29e05bde4adeb718278cb063a400a
emrekardaslar <emre.kardaslar@gmail.com>
2023-05-21 12:14:57 +0300

    add ride options component details

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,10 +1,92 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import tw from "tailwind-react-native-classnames";
+import { Icon, Image } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image:
+      "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_637/v1596627972/assets/e7/e861a8-30ec-4d57-8045-7186f6c5ec35/original/comfort.png",
+  },
+  {
+    id: "Uber-X-456",
+    title: "UberXL",
+    multiplier: 1.2,
+    image:
+      "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_637/v1555367538/assets/31/ad21b7-595c-42e8-ac53-53966b4a5fee/original/Final_Black.png",
+  },
+  {
+    id: "Uber-X-789",
+    title: "UberLUX",
+    multiplier: 1.75,
+    image:
+      "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_637/v1591275848/assets/57/348090-0115-46b7-96b3-b9085e736876/original/Comfort_Vehicle_list.png",
+  },
+];
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+
   return (
-    <View>
-      <Text>Pick a ride</Text>
+    <View style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+          onPress={() => navigation.navigate("NavigateCard")}
+        >
+          <Icon name="chevron-left" type="font-awesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row items-center justify-between px-10 ${
+              item.id === selected?.id && "bg-gray-200"
+            }`}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: item.image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{item.title}</Text>
+              <Text>Travel Time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$10</Text>
+          </TouchableOpacity>
+        )}
+      />
+
+      <View>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+        >
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
